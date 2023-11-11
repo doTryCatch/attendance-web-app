8f78761ab14e0a4c16eb1f04433c4e0325856a52
doTryCatch <rpsuper1640@gmail.com>
2023-11-11 19:49:49 +0530

    dynamic routing fixed

--- cliented/pages/[section].js
+++ cliented/pages/[section].js
@@ -1,6 +1,75 @@
-const routing=({props})=>{
-  console.log({props})
-  return <h1 className="text-white">{props}</h1>;
 
+// components/Layout.js
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Home from "./components/home";
+import Attendence from "./components/attendence";
+import Register from "./components/register";
+import Report from "./components/report"
+import withAuth from "./auth/withAuth"
+
+
+
+
+function Layout({ children }) {
+  const router = useRouter();
+  const section=router.query.section
+  const [login, setLogin] = useState(true);
+
+  const [activeComponent, setActiveComponent] = useState("Home");
+  const [path, setPath] = useState("/dashboard");
+  const handleComponentChange = (componentName) => {
+    router.push("/"+componentName)
+  
+   };
+useEffect(()=>{
+  console.log(section)
+  if(section){
+    setActiveComponent(section)
+  }else{
+    setActiveComponent("/")
+  }
+
+},[section])
+
+
+
+  return (
+    <>
+      {login ? (
+        <section className="dashboard">
+          <div className="navbar flex text-bold bg-slate-500 cursor-pointer my-2 text-white">
+            <div className="list-items">
+              <ul className="flex space-x-6">
+                <li onClick={() => handleComponentChange("")}>Home</li>
+                <li onClick={() => handleComponentChange("Report")}>
+                  Report
+                </li>
+                <li onClick={() => handleComponentChange("Attendence")}>
+                  Attendence
+                </li>
+                <li onClick={() => handleComponentChange("Register")}>
+                  Register
+                </li>
+              </ul>
+            </div>
+          </div>
+       
+          <div>
+            {activeComponent === "Report" && <Report />}
+            {activeComponent === "Attendence" && <Attendence />}
+            {activeComponent === "Register" && <Register />}
+            {activeComponent==="/"&& <Home/>}
+          </div>
+          {/* ) : (
+        children
+      )} */}
+        </section>
+      ) : (
+        <p>loading</p>
+      )}
+    </>
+  );
 }
-export default routing
+
+export default withAuth(Layout);
--- cliented/pages/index.tsx
+++ cliented/pages/index.tsx
@@ -1,61 +1,11 @@
-// components/Layout.js
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import Home from "./components/home";
-import Attendence from "./components/attendence";
-import Register from "./components/register";
-import withAuth from "./auth/withAuth"
 
+import React from 'react'
+import Section from "./[section]"
 
-
-function Layout({ children }) {
-  const router = useRouter();
-  const [login, setLogin] = useState(true);
-
-  const [activeComponent, setActiveComponent] = useState("Home");
-  const [path, setPath] = useState("/dashboard");
-
-
-  const handleComponentChange = (componentName) => {
-    const newUrl = `/${componentName}`;
-    window.history.pushState({}, "", newUrl);
-    setActiveComponent(componentName);
- 
-  };
-
-
+function App() {
   return (
-    <>
-      {login ? (
-        <section className="dashboard">
-          <div className="navbar flex text-bold bg-slate-500 cursor-pointer my-2 text-white">
-            <div className="list-items">
-              <ul className="flex space-x-6">
-                <li onClick={() => handleComponentChange("Home")}>Home</li>
-                <li onClick={() => handleComponentChange("Attendence")}>
-                  Attendence
-                </li>
-                <li onClick={() => handleComponentChange("Register")}>
-                  Register
-                </li>
-              </ul>
-            </div>
-          </div>
-          {/* {router.pathname === '/dashboard' ? ( */}
-          <div>
-            {activeComponent === "Home" && <Home />}
-            {activeComponent === "Attendence" && <Attendence />}
-            {activeComponent === "Register" && <Register />}
-          </div>
-          {/* ) : (
-        children
-      )} */}
-        </section>
-      ) : (
-        <p>loading</p>
-      )}
-    </>
-  );
+   <Section/>
+  )
 }
 
-export default withAuth(Layout);
+export default App
